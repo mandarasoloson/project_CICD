2eef81991b4a73cdda6c97342bedc0a175fb9561
rasoloson <manda.rasoloson@etu.unistra.fr>
2026-05-06 16:27:36 +0200

    chore(setup): initialisation du projet avec typescript, eslint et husky

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/explicit-function-return-type": "warn"
+    }
+  }
+);
