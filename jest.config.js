a7bf61100616d6e0376ae2daf30b818555c18d7e
rasoloson <manda.rasoloson@etu.unistra.fr>
2026-05-28 11:16:20 +0200

    ci: ajout du typecheck TypeScript, seuil de coverage et job CD vers GHCR

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
     '!src/utils/logger.ts' // On ne teste pas la console pure
   ],
   coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
